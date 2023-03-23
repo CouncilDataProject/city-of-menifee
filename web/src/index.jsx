619cb9eaa8d879e69d47175148c1ac49b3e9be4f
evamaxfield <evamaxfieldbrown@gmail.com>
2023-03-23 15:02:54 -0700

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-city-of-menifee-yjlhhbdn",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "City of Menifee",
+        timeZone: "America/Los_Angeles",
+        footerLinksSections: [],
+    },
+    features: {
+        // enableClipping: true,
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
